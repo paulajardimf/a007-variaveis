a07579218cdd562836688ee033aca51284235429
Paula <paulajardimf@gmail.com>
2022-08-29 13:10:33 -0300

    exercício 1 finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,13 @@
+// const nome = null;
+// let idade = null;
+
 nome = prompt("Qual o seu nome?");
 idade = prompt("Qual a sua idade?");
 
 console.log(typeof nome, nome);
 console.log(typeof idade, idade);
+
+console.log(`Olá ${nome}, você tem ${idade} anos.`);
 
 // d) retornou ambos os tipos como objeto
 // f) retornou ambos os tipos como string pois foram inseridas pelo prompt
