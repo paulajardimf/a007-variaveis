e7af9e4187a1b6e5d098ce1e6389945edc1bfaf7
Paula <paulajardimf@gmail.com>
2022-08-29 12:54:33 -0300

    resolução do exercício letra f

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,8 @@
-const nome = null;
-let idade = null;
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
 
 console.log(typeof nome, nome);
 console.log(typeof idade, idade);
 
 // d) retornou ambos os tipos como objeto
+// f) retornou ambos os tipos como string pois foram inseridas pelo prompt
