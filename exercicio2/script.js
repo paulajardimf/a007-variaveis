0666b94e82487bf88eca7edebb864517c3b09b6e
Paula <paulajardimf@gmail.com>
2022-08-29 14:40:54 -0300

    exercicio 2 primeira etapa

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+corAzul = prompt("Você está usando alguma peça de roupa com a cor azul?");
+
+corBranco = prompt("Você está usando alguma peça de roupa com a cor branco?");
+
+corPreto = prompt("Você está usando alguma peça de roupa com a cor preto?");
+
+console.log(corAzul, corBranco, corPreto);
